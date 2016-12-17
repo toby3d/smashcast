19522e13dda1d7fddf9ddaa4c645e0eb5d08e131
Maxim Lebedev <mail@toby3d.ru>
2016-12-18 04:16:26 +0500

    ✅ Approved checkFollowingStatus

--- checkFollowingStatus.go
+++ checkFollowingStatus.go
@@ -3,38 +3,42 @@ package hitGox
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/valyala/fasthttp"
 )
 
-type (
-	// FollowingStatus is a response body.
-	FollowingStatus struct {
-		Following Follower `json:"following"`
-	}
-
-	// Follower show information about following status.
-	Follower struct {
+// FollowingStatus is about follower relationship.
+type FollowingStatus struct {
+	Following struct {
 		FollowID       string `json:"follow_id"`
 		FollowerUserID string `json:"follower_user_id"`
-
-		// The follower_notify property shows whether that user has email notification turned on 1 or not 0.
-		FollowerNotify string `json:"follower_notify"`
-	}
-)
+		FollowerNotify string `json:"follower_notify"` // The follower_notify property shows whether that user has email notification turned on 1 or not 0.
+	} `json:"following"`
+}
 
 // CheckFollowingStatus returns follower relationship from userName to channel.
-func CheckFollowingStatus(userName UserName, channel string) (FollowingStatus, error) {
-	var args fasthttp.Args
-	args.Add("user_name", userName.UserName)
-	requestURL := fmt.Sprintf("%s/following/user/%s?%s", API, channel, args.String())
-	_, resp, err := fasthttp.Get(nil, requestURL)
-	if err != nil {
-		return FollowingStatus{}, err
+func CheckFollowingStatus(userName string, channel string) (*FollowingStatus, error) {
+	switch {
+	case userName == "":
+		return nil, errors.New("username can not be empty")
+	case channel == "":
+		return nil, errors.New("channel can not be empty")
 	}
+
+	var args fasthttp.Args
+	args.Add("user_name", userName)
+
+	url := fmt.Sprintf("%s/following/user/%s?%s", APIEndpoint, channel, args.String())
+	_, resp, err := fasthttp.Get(nil, url)
+	if err != nil {
+		return nil, err
+	}
+
 	var obj FollowingStatus
 	if err = json.NewDecoder(bytes.NewReader(resp)).Decode(&obj); err != nil {
-		return FollowingStatus{}, err
+		return nil, err
 	}
-	return obj, nil
+
+	return &obj, nil
 }
