59a4262a574dd9414bb70fbdf30060b8ca932b63
Maxim Lebedev <mail@toby3d.ru>
2016-12-18 04:43:03 +0500

    ✅ Approved getToken

--- getToken.go
+++ getToken.go
@@ -3,26 +3,41 @@ package hitGox
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"github.com/valyala/fasthttp"
 )
 
+// AuthToken is about generated key for most actions by user.
 type AuthToken struct {
-	AuthToken string `json:"authToken,omitempty"`
+	AuthToken string `json:"authToken"`
 }
 
-// GetToken get an authentication authToken rather than account information.
-func GetToken(login string, pass string, app Application) (AuthToken, error) {
-	args := fasthttp.AcquireArgs()
+// GetToken can be used alternatively to just get an authentication token rather than account information.
+func (app *Application) GetToken(login string, pass string) (*AuthToken, error) {
+	switch {
+	case app.Name == "":
+		return nil, errors.New("no name of application, create new application first")
+	case login == "":
+		return nil, errors.New("login can not be empty")
+	case pass == "":
+		return nil, errors.New("pass can not be empty")
+	}
+
+	var args fasthttp.Args
 	args.Add("login", login)
 	args.Add("pass", pass)
 	args.Add("app", app.Name)
-	statusCode, body, err := fasthttp.Post(nil, API+"/auth/token", args)
-	if statusCode != 200 || err != nil {
-		return AuthToken{}, err
+
+	url := APIEndpoint + "/auth/token"
+	_, resp, err := fasthttp.Post(nil, url, &args)
+	if err != nil {
+		return nil, err
 	}
+
 	var obj AuthToken
-	if err = json.NewDecoder(bytes.NewReader(body)).Decode(&obj); err != nil {
-		return AuthToken{}, err
+	if err = json.NewDecoder(bytes.NewReader(resp)).Decode(&obj); err != nil {
+		return nil, err
 	}
-	return obj, nil
+
+	return &obj, nil
 }
