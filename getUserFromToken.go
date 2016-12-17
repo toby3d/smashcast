fa1817575c1c55bb522d3edc98ad65481a6f09b1
Maxim Lebedev <mail@toby3d.ru>
2016-12-18 04:32:54 +0500

    ✅ Approved getUserFromToken

--- getUserFromToken.go
+++ getUserFromToken.go
@@ -3,25 +3,32 @@ package hitGox
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/valyala/fasthttp"
 )
 
-// UserName is a hitbox Username.
+// UserName is about user username.
 type UserName struct {
-	UserName string `json:"user_name,omitempty"`
+	UserName string `json:"user_name"`
 }
 
 // GetUserFromToken returns user associated with authToken.
-func GetUserFromToken(authToken AuthToken) (UserName, error) {
-	requestURL := fmt.Sprintf("%s/userfromauthToken/%s", API, authToken.AuthToken)
-	_, body, err := fasthttp.Get(nil, requestURL)
+func GetUserFromToken(authToken string) (*UserName, error) {
+	if authToken == "" {
+		return nil, errors.New("this action requires a authtoken")
+	}
+
+	url := fmt.Sprintf("%s/userfromtoken/%s", APIEndpoint, authToken)
+	_, resp, err := fasthttp.Get(nil, url)
 	if err != nil {
-		return UserName{}, err
+		return nil, err
 	}
+
 	var obj UserName
-	if err = json.NewDecoder(bytes.NewReader(body)).Decode(&obj); err != nil {
-		return UserName{}, err
+	if err = json.NewDecoder(bytes.NewReader(resp)).Decode(&obj); err != nil {
+		return nil, err
 	}
-	return obj, nil
+
+	return &obj, nil
 }
