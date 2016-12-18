d8a87fb7bbbc46e82b154a2425e070d27fa2a0a9
Maxim Lebedev <mail@toby3d.ru>
2016-12-18 05:03:03 +0500

    ✅ Approved exchange

--- exchange.go
+++ exchange.go
@@ -4,31 +4,43 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"github.com/valyala/fasthttp"
 )
 
-// AccessToken is the equivalent of a regular authToken. You are now able to use this on the hitbox API just like any other authToken.
+// AccessToken is the equivalent of a regular authToken. You are now able to use this on the hitbox API just like any other token.
 type AccessToken struct {
-	AccessToken string `json:"access_token,omitempty"`
+	AccessToken string `json:"access_token"`
 }
 
-// Exchange get an authentication authToken rather than account information.
-//
-// The hash value is a Base64 encode of the app.Token and app.Secret. As an example, you can open up the Chrome/Firefox Developer Tool, go to the console and type btoa("app_authToken"+"app_secret"); and the result would be your hash.
-func (app Application) Exchange(requestToken string) (AccessToken, error) {
+// Exchange the exchange request_token for an authToken once the user is redirected.
+func (app *Application) Exchange(requestToken string) (*AccessToken, error) {
+	switch {
+	case app.Token == "":
+		return nil, errors.New("no token of application, create new application first")
+	case app.Secret == "":
+		return nil, errors.New("no secret of application, create new application first")
+	case requestToken == "":
+		return nil, errors.New("requesttoken can not be empty")
+	}
+
 	hash := base64.StdEncoding.EncodeToString([]byte(app.Token + app.Secret))
 
-	args := fasthttp.AcquireArgs()
+	var args fasthttp.Args
 	args.Add("request_token", requestToken)
 	args.Add("app_token", app.Token)
 	args.Add("hash", hash)
-	statusCode, body, err := fasthttp.Post(nil, API+"/oauth/exchange", args)
-	if statusCode != 200 || err != nil {
-		return AccessToken{}, err
+
+	url := APIEndpoint + "/oauth/exchange"
+	_, resp, err := fasthttp.Post(nil, url, &args)
+	if err != nil {
+		return nil, err
 	}
+
 	var obj AccessToken
-	if err = json.NewDecoder(bytes.NewReader(body)).Decode(&obj); err != nil {
-		return AccessToken{}, err
+	if err = json.NewDecoder(bytes.NewReader(resp)).Decode(&obj); err != nil {
+		return nil, err
 	}
-	return obj, nil
+
+	return &obj, nil
 }
