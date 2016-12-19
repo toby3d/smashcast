8b52fc8520dc2d04182792b48c64f21cd17a0c94
Maxim Lebedev <mail@toby3d.ru>
2016-12-19 21:46:45 +0500

    ✅ Updated getIngests

---
+++ getIngests.go
@@ -0,0 +1,28 @@
+package hitGox
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+// Ingest is a single list element about streaming server for programs.
+type Ingest []struct {
+	IngestLocation string `json:"ingest_location"`
+	IngestURL      string `json:"ingest_url"`
+}
+
+// GetIngests returns a list of servers for live stream ingesting.
+func GetIngests() (Ingest, error) {
+	url := APIEndpoint + "/ingests/default_list"
+	resp, err := get(url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var obj Ingest
+	if err = json.NewDecoder(bytes.NewReader(resp)).Decode(&obj); err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
