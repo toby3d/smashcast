ef5c6951bee6d0ac56be7d87b6d381efc6f153ea
Maxim Lebedev <mail@toby3d.ru>
2016-12-19 21:26:18 +0500

    ✅ Approved resetStreamKey

--- resetStreamKey.go
+++ resetStreamKey.go
@@ -1,15 +1,38 @@
 package hitGox
 
-/*
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"github.com/valyala/fasthttp"
 )
 
-// ResetStreamKey get the stream key for channel.
-func ResetStreamKey(channel string, authToken AuthToken) (StreamKey, error) {
+// ResetStreamKey sets a new stream key for channel.
+//
+// Editors can run this API.
+func (account *Account) ResetStreamKey(channel string) (string, error) {
+	switch {
+	case account.AuthToken == "":
+		return "", errors.New("authtoken in account can not be empty")
+	case channel == "":
+		return "", errors.New("channel can not be empty")
+	}
 
+	var args fasthttp.Args
+	args.Add("authToken", account.AuthToken)
+
+	url := APIEndpoint + "/mediakey/" + channel
+	resp, err := put(nil, url, &args)
+	if err != nil {
+		return "", err
+	}
+
+	var obj = struct {
+		StreamKey string `json:"streamKey"`
+	}{}
+	if err = json.NewDecoder(bytes.NewReader(resp)).Decode(&obj); err != nil {
+		return "", err
+	}
+
+	return obj.StreamKey, nil
 }
-*/
