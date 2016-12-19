2340e9e31a91250752dfc18bb6a5f0ecf44bd034
Maxim Lebedev <mail@toby3d.ru>
2016-12-19 22:00:27 +0500

    ✅ Updated getGame

--- getGame.go
+++ getGame.go
@@ -3,42 +3,60 @@ package hitGox
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/valyala/fasthttp"
+	"strconv"
 )
 
-// Game is a response body.
+// Game is about category of streams.
 type Game struct {
-	Request    Request  `json:"request"`
-	Categories Category `json:"category"`
+	Request struct {
+		This string `json:"this"`
+	} `json:"request"`
+	Category struct {
+		CategoryID         string      `json:"category_id"`
+		CategoryName       string      `json:"category_name"`
+		CategoryNameShort  interface{} `json:"category_name_short"`
+		CategorySeoKey     string      `json:"category_seo_key"`
+		CategoryViewers    string      `json:"category_viewers"`
+		CategoryMediaCount string      `json:"category_media_count"`
+		CategoryChannels   interface{} `json:"category_channels"`
+		CategoryLogoSmall  interface{} `json:"category_logo_small"`
+		CategoryLogoLarge  string      `json:"category_logo_large"`
+		CategoryUpdated    string      `json:"category_updated"`
+	} `json:"category"`
 }
 
-// GetGameByID return information about game category by gameID (eg. 1)
-func GetGameByID(gameID int) (Game, error) {
-	requestURL := fmt.Sprintf("%s/game/%d", API, gameID)
-	_, body, err := fasthttp.Get(nil, requestURL)
-	if err != nil {
-		return Game{}, err
-	}
-	var obj Game
-	if err = json.NewDecoder(bytes.NewReader(body)).Decode(&obj); err != nil {
-		return Game{}, err
-	}
-	return obj, nil
-}
+// GetGame return information about game category.
+func GetGame(game interface{}) (*Game, error) {
+	var seo bool
 
-// GetGameByKey return information about game category by `gameKey` (eg. league-of-legends)
-func GetGameByKey(gameKey string) (Game, error) {
+	switch game.(type) {
+	case string:
+		if game == "" {
+			return nil, errors.New("game can not be empty")
+		}
+		seo = true
+	case int:
+		seo = false
+	default:
+		return nil, errors.New("game mast be only as string or int")
+	}
+
 	var args fasthttp.Args
-	args.Add("seo", "true") // If using a game name, this must be true.
-	requestURL := fmt.Sprintf("%s/game/%s?%s", API, gameKey, args.String())
-	_, body, err := fasthttp.Get(nil, requestURL)
+	args.Add("seo", strconv.FormatBool(seo))
+
+	url := fmt.Sprint(APIEndpoint, "/game/", game)
+	resp, err := get(url, &args)
 	if err != nil {
-		return Game{}, err
+		return nil, err
 	}
+
 	var obj Game
-	if err = json.NewDecoder(bytes.NewReader(body)).Decode(&obj); err != nil {
-		return Game{}, err
+	if err = json.NewDecoder(bytes.NewReader(resp)).Decode(&obj); err != nil {
+		return nil, err
 	}
-	return obj, nil
+
+	return &obj, nil
 }
