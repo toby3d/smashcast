bcb951bf47e887327584601528adb600ed971817
Maxim Lebedev <mail@toby3d.ru>
2016-12-19 17:34:46 +0500

    ✅ Approved followAChannel

---
+++ followAChannel.go
@@ -0,0 +1,45 @@
+package hitGox
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/valyala/fasthttp"
+)
+
+// FollowAChannel follows a channel.
+//
+// id can be either a username or user_id of a user you want to follow.
+func (account *Account) FollowAChannel(id string) (*Status, error) {
+	switch {
+	case account.AuthToken == "":
+		return nil, errors.New("authtoken in account can not be empty")
+	case id == "":
+		return nil, errors.New("id can not be empty")
+	}
+
+	var changes = struct {
+		Type     string `json:"type"`
+		FollowID string `json:"follow_id"`
+	}{"user", id}
+
+	dst, err := json.Marshal(changes)
+	if err != nil {
+		return nil, err
+	}
+
+	var args fasthttp.Args
+	args.Add("authToken", account.AuthToken)
+
+	url := APIEndpoint + "/follow"
+	resp, err := post(dst, url, &args)
+	if err != nil {
+		return nil, err
+	}
+
+	status, err := stupidFuckingStatusResponseByLazyAPIDevelopers(&resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return status, nil
+}
